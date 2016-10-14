aa4a87ab992685c423dcd37f67c3e5ef5f707345
Gerald <Gerald@GERALD-PC>
2016-10-14 04:33:42 -0600

    BusinessLogic de Empleado

--- BusinessLogic/Employee.cs
+++ BusinessLogic/Employee.cs
@@ -25,7 +25,69 @@ namespace BusinessLogic
 
         public void AddEmployee()
         {
+            TOEmployee employee = new TOEmployee();
+            TODepartment department = new TODepartment();
+            DAEmployee dae = new DAEmployee();
 
+            employee.Code = this.Code;
+            employee.Name = this.Name;
+            employee.LastName = this.LastName;
+            department.Name = this.Department.Name;
+            department.Code = this.Department.Code;
+            employee.Departament = department;
+
+            dae.AddEmployee(employee);
+
+        }
+
+        public void DeleteEmployee()
+        {
+            TOEmployee employee = new TOEmployee();
+            DAEmployee dae = new DAEmployee();
+
+            employee.Code = this.Code;
+
+            dae.DeleteEmployee(employee);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            DAEmployee dae = new DAEmployee();
+            List<Employee> employees = new List<Employee>();
+            List<TOEmployee> list = dae.GetAllEmployees();
+            foreach(TOEmployee e in list)
+            {
+                Employee employee = new Employee(e.Code, e.Name, e.LastName, new Department(e.Departament.Code, e.Departament.Name));
+                employees.Add(employee);
+            }
+
+            return employees;
+        }
+
+        public Employee GetEmployee()
+        {
+
+            DAEmployee dae = new DAEmployee();
+            TOEmployee e = dae.GetEmployee(this.Code);
+            Employee employee = new Employee(e.Code, e.Name, e.LastName, new Department(e.Departament.Code, e.Departament.Name));
+
+            return employee;
+        }
+
+        public void ModifyEmployee()
+        {
+            TOEmployee employee = new TOEmployee();
+            TODepartment department = new TODepartment();
+            DAEmployee dae = new DAEmployee();
+
+            employee.Code = this.Code;
+            employee.Name = this.Name;
+            employee.LastName = this.LastName;
+            department.Name = this.Department.Name;
+            department.Code = this.Department.Code;
+            employee.Departament = department;
+
+            dae.ModifyEmployee(employee);
         }
     }
 }
