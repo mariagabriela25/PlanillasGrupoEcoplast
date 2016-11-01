8c67b720f2694df3b2ab91c17e584ca4800bd160
Jose <Jose@JOSE-PC>
2016-11-01 03:13:03 -0600

    Cambios en DaoWeekDetail

--- DataAccess/DAOWeekDetail.cs
+++ DataAccess/DAOWeekDetail.cs
@@ -11,7 +11,7 @@ namespace DataAccess
 {
     public class DAOWeekDetail
     {
-        SqlConnection conex = new SqlConnection(DataAccess.Properties.Settings.Default.StringConex);
+        SqlConnection conex = new SqlConnection(DataAccess.Properties.Settings.Default.StringConexAnviz);
 
         public Boolean SaveWeekDetail(TOWorkWeekDetail twd)
         {
@@ -86,10 +86,12 @@ namespace DataAccess
 
             if (reader.HasRows)
             {
-                reader.Read();
-                int week = reader.GetInt32(0);
+                while (reader.Read())
+                {
+                    int week = reader.GetInt32(0);
 
-                weeks.Add(week);
+                    weeks.Add(week);
+                }
             }
 
             if (conex.State != ConnectionState.Closed)
