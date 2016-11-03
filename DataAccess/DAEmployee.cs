b068fff052917789f9759fbc90027700759c654e
Cristian <Cristian>
2016-11-02 23:05:07 -0600

    validaciones DAO s

--- DataAccess/DAEmployee.cs
+++ DataAccess/DAEmployee.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using TransferObjects;
+using System.Windows.Forms;
 
 namespace DataAccess
 {
@@ -17,75 +18,113 @@ namespace DataAccess
 
         public void AddEmployee(TOEmployee employee)
         {
-            SqlCommand query = new SqlCommand("INSERT INTO Empleado VALUES (@CodEmpleado, @Nombre, @Apellido, @CodDepartamento)", conex);
-            query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
-            query.Parameters.AddWithValue("@Nombre", employee.Name);
-            query.Parameters.AddWithValue("@Apellido", employee.LastName);
-            query.Parameters.AddWithValue("@CodDepartamento", employee.Departament.Code);
-
-            if (conex.State != ConnectionState.Open)
+            try
             {
-                conex.Open();
+
+                SqlCommand query = new SqlCommand("INSERT INTO Empleado VALUES (@CodEmpleado, @Nombre, @Apellido, @CodDepartamento)", conex);
+                query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
+                query.Parameters.AddWithValue("@Nombre", employee.Name);
+                query.Parameters.AddWithValue("@Apellido", employee.LastName);
+                query.Parameters.AddWithValue("@CodDepartamento", employee.Departament.Code);
+
+                if (conex.State != ConnectionState.Open)
+                {
+                    conex.Open();
+                }
+
+                query.ExecuteNonQuery();
+
             }
-
-            query.ExecuteNonQuery();
-
-            if (conex.State != ConnectionState.Closed)
+            catch (Exception ex)
             {
-                conex.Close();
+                MessageBox.Show("Error de conexión");
+            }
+            finally
+            {
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
         }
 
         public void DeleteEmployee(TOEmployee employee)
         {
-            SqlCommand query =  new SqlCommand("DELETE FROM Empleado WHERE CodEMpleado = @CodEmpleado", conex);
-            query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
-
-            if (conex.State != ConnectionState.Open)
+            try
             {
-                conex.Open();
+
+                SqlCommand query = new SqlCommand("DELETE FROM Empleado WHERE CodEMpleado = @CodEmpleado", conex);
+                query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
+
+                if (conex.State != ConnectionState.Open)
+                {
+                    conex.Open();
+                }
+
+                query.ExecuteNonQuery();
+
+                if (conex.State != ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
-
-            query.ExecuteNonQuery();
-
-            if (conex.State != ConnectionState.Closed)
+            catch (Exception ex)
             {
-                conex.Close();
+                MessageBox.Show("Error de conexión");
+            }
+            finally
+            {
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
         }
-        
+
         public List<TOEmployee> GetAllEmployees()
         {
             List<TOEmployee> employees = new List<TOEmployee>();
-            SqlCommand query = new SqlCommand("SELECT Empleado.*, NombreDepartamento FROM Empleado, Departamento WHERE Departamento.CodDepartamento = Empleado.CodDepartamento;", conex);
 
-            if (conex.State != ConnectionState.Open)
-            {
-                conex.Open();
-            }
-            SqlDataReader reader = query.ExecuteReader();
-
-            if(reader.HasRows)
+            try
             {
 
-                while (reader.Read())
+                SqlCommand query = new SqlCommand("SELECT Empleado.*, NombreDepartamento FROM Empleado, Departamento WHERE Departamento.CodDepartamento = Empleado.CodDepartamento;", conex);
+
+                if (conex.State != ConnectionState.Open)
                 {
-                    TOEmployee e = new TOEmployee();
-                    TODepartment d = new TODepartment();
-                    e.Code = reader.GetInt32(0);
-                    e.Name = reader.GetString(1);
-                    e.LastName = reader.GetString(2);
-                    d.Code = reader.GetInt32(3);
-                    d.Name = reader.GetString(4);
-                    e.Departament = d;
-
-                    employees.Add(e);
+                    conex.Open();
                 }
-            }
+                SqlDataReader reader = query.ExecuteReader();
 
-            if (conex.State != ConnectionState.Closed)
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        TOEmployee e = new TOEmployee();
+                        TODepartment d = new TODepartment();
+                        e.Code = reader.GetInt32(0);
+                        e.Name = reader.GetString(1);
+                        e.LastName = reader.GetString(2);
+                        d.Code = reader.GetInt32(3);
+                        d.Name = reader.GetString(4);
+                        e.Departament = d;
+
+                        employees.Add(e);
+                    }
+                }
+
+            }
+            catch (Exception ex)
             {
-                conex.Close();
+                MessageBox.Show("Error de conexión");
+            }
+            finally
+            {
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
 
 
@@ -95,33 +134,43 @@ namespace DataAccess
         public TOEmployee GetEmployee(int Code)
         {
             TOEmployee employee = new TOEmployee();
-
-            SqlCommand query = new SqlCommand("SELECT e.*, NombreDepartamento FROM Empleado e INNER JOIN Departamento d ON e.CodDepartamento = d.CodDepartamento WHERE CodEmpleado = @Code;", conex);
-            query.Parameters.AddWithValue("@Code", Code);
-
-            if (conex.State != ConnectionState.Open)
+            try
             {
-                conex.Open();
+
+                SqlCommand query = new SqlCommand("SELECT e.*, NombreDepartamento FROM Empleado e INNER JOIN Departamento d ON e.CodDepartamento = d.CodDepartamento WHERE CodEmpleado = @Code;", conex);
+                query.Parameters.AddWithValue("@Code", Code);
+
+                if (conex.State != ConnectionState.Open)
+                {
+                    conex.Open();
+                }
+
+                SqlDataReader reader = query.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+
+                    TODepartment d = new TODepartment();
+                    employee.Code = reader.GetInt32(0);
+                    employee.Name = reader.GetString(1);
+                    employee.LastName = reader.GetString(2);
+                    d.Code = reader.GetInt32(3);
+                    d.Name = reader.GetString(4);
+                    employee.Departament = d;
+                }
+
             }
-
-            SqlDataReader reader = query.ExecuteReader();
-
-            if(reader.HasRows)
+            catch (Exception ex)
             {
-                reader.Read();
-
-                TODepartment d = new TODepartment();
-                employee.Code = reader.GetInt32(0);
-                employee.Name = reader.GetString(1);
-                employee.LastName = reader.GetString(2);
-                d.Code = reader.GetInt32(3);
-                d.Name = reader.GetString(4);
-                employee.Departament = d;
+                MessageBox.Show("Error de conexión");
             }
-
-            if (conex.State != ConnectionState.Closed)
+            finally
             {
-                conex.Close();
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
 
             return employee;
@@ -129,22 +178,33 @@ namespace DataAccess
 
         public void ModifyEmployee(TOEmployee employee)
         {
-            SqlCommand query = new SqlCommand("UPDATE Empleado SET CodDepartamento = @CodDepartamento, Nombre = @Nombre, Apellido = @Apellido WHERE CodEmpleado = @CodEmpleado;", conex);
-            query.Parameters.AddWithValue("@CodDepartamento", employee.Departament.Code);
-            query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
-            query.Parameters.AddWithValue("@Nombre", employee.Name);
-            query.Parameters.AddWithValue("@Apellido", employee.LastName);
-            
-            if (conex.State != ConnectionState.Open)
+            try
             {
-                conex.Open();
+
+                SqlCommand query = new SqlCommand("UPDATE Empleado SET CodDepartamento = @CodDepartamento, Nombre = @Nombre, Apellido = @Apellido WHERE CodEmpleado = @CodEmpleado;", conex);
+                query.Parameters.AddWithValue("@CodDepartamento", employee.Departament.Code);
+                query.Parameters.AddWithValue("@CodEmpleado", employee.Code);
+                query.Parameters.AddWithValue("@Nombre", employee.Name);
+                query.Parameters.AddWithValue("@Apellido", employee.LastName);
+
+                if (conex.State != ConnectionState.Open)
+                {
+                    conex.Open();
+                }
+
+                query.ExecuteNonQuery();
+
             }
-
-            query.ExecuteNonQuery();
-
-            if (conex.State != ConnectionState.Closed)
+            catch (Exception ex)
             {
-                conex.Close();
+                MessageBox.Show("Error de conexión");
+            }
+            finally
+            {
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
 
         }
@@ -152,39 +212,51 @@ namespace DataAccess
 
         public List<TOEmployee> GetEmployeesDepartment(int departmentCode)
         {
+
             List<TOEmployee> employees = new List<TOEmployee>();
-            SqlCommand query = new SqlCommand("SELECT * FROM Empleado inner join Departamento ON Empleado.CodDepartamento = Departamento.CodDepartamento WHERE Empleado.CodDepartamento = @depCode;", conex);
-            query.Parameters.AddWithValue("@depCode", departmentCode);
 
-            if (conex.State != ConnectionState.Open)
-            {
-                conex.Open();
-            }
-            SqlDataReader reader = query.ExecuteReader();
-
-            if (reader.HasRows)
+            try
             {
 
-                while (reader.Read())
+                SqlCommand query = new SqlCommand("SELECT * FROM Empleado inner join Departamento ON Empleado.CodDepartamento = Departamento.CodDepartamento WHERE Empleado.CodDepartamento = @depCode;", conex);
+                query.Parameters.AddWithValue("@depCode", departmentCode);
+
+                if (conex.State != ConnectionState.Open)
                 {
-                    TOEmployee e = new TOEmployee();
-                    TODepartment d = new TODepartment();
-                    e.Code = reader.GetInt32(0);
-                    e.Name = reader.GetString(1);
-                    e.LastName = reader.GetString(2);
-                    d.Code = reader.GetInt32(3);
-                    d.Name = reader.GetString(4);
-                    e.Departament = d;
+                    conex.Open();
+                }
+                SqlDataReader reader = query.ExecuteReader();
 
-                    employees.Add(e);
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        TOEmployee e = new TOEmployee();
+                        TODepartment d = new TODepartment();
+                        e.Code = reader.GetInt32(0);
+                        e.Name = reader.GetString(1);
+                        e.LastName = reader.GetString(2);
+                        d.Code = reader.GetInt32(3);
+                        d.Name = reader.GetString(4);
+                        e.Departament = d;
+
+                        employees.Add(e);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión");
+            }
+            finally
+            {
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
                 }
             }
-
-            if (conex.State != ConnectionState.Closed)
-            {
-                conex.Close();
-            }
-
 
             return employees;
         }
@@ -192,26 +264,36 @@ namespace DataAccess
         public double GetTotalHoursPerWeek(int code, int weeknumber)
         {
 
-            SqlCommand query = new SqlCommand("SELECT SUM(TotalHoras) FROM DetalleDiaLaborado Where CodEmpleado = @employee AND CodSemana = @week", conex);
-            query.Parameters.AddWithValue("@employee", code);
-            query.Parameters.AddWithValue("@week", weeknumber);
-
-            if (conex.State != ConnectionState.Open)
+            try
             {
-                conex.Open();
+                SqlCommand query = new SqlCommand("SELECT SUM(TotalHoras) FROM DetalleDiaLaborado Where CodEmpleado = @employee AND CodSemana = @week", conex);
+                query.Parameters.AddWithValue("@employee", code);
+                query.Parameters.AddWithValue("@week", weeknumber);
+
+                if (conex.State != ConnectionState.Open)
+                {
+                    conex.Open();
+                }
+
+                SqlDataReader reader = query.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    return (Double)reader.GetDecimal(0);
+                }
+
             }
-
-            SqlDataReader reader = query.ExecuteReader();
-
-            if(reader.HasRows)
+            catch (Exception ex)
             {
-                reader.Read();
-                return (Double)reader.GetDecimal(0);
+                MessageBox.Show("Error de conexión");
             }
-
-            if (conex.State != ConnectionState.Closed)
+            finally
             {
-                conex.Close();
+                if (conex.State != System.Data.ConnectionState.Closed)
+                {
+                    conex.Close();
+                }
             }
             return 0;
         }
