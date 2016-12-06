ed923ee8c76c61a4ca9ca0289e41a85a1266e7ee
Cristian <Cristian@DESKTOP-Q96TJBA>
2016-12-06 12:10:02 -0600

    algunas pruebas al dao

---
+++ UnitTestProject/DAUser_Tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Tests
+{
+    [TestClass()]
+    public class DAUser_Tests
+    {
+        [TestMethod()]
+        public void grantAccessTest()
+        {
+            DAOuser user = new DAOuser();
+
+            Boolean expect = true; 
+            Boolean actual;
+
+            actual = user.grantAccess("pass");
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void ChangePsw()
+        {
+            DAOuser user = new DAOuser();
+
+            Boolean expect = true; 
+            Boolean actual;
+
+            actual = user.ChangePsw("passold", "passnew");//poner el codigo del emp y la semana laborada
+
+            Assert.AreEqual(expect, actual);
+        }
+
+
+    }
+}
